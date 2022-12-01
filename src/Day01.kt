575e9c1f3e61e19fed91b99d5547aa7b671d5d4f
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2022-12-01 11:37:34 +0100

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
+data class Elf(val i: Int, val calories: Int)
+
+fun Elf(i: Int, items: List<String>) = Elf(i, items.sumOf { it.toInt() })
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun sortedElves(input: List<String>): List<Elf> {
+        val elves: MutableList<Elf> = mutableListOf()
+        var from = 0
+        while (from < input.size) {
+            val to = input.subList(from, input.size).indexOfFirst { it.isEmpty() }
+            if (to == -1) break
+            elves.add(Elf(elves.size + 1, input.subList(from, from + to)))
+            from += to + 1
+        }
+        return elves.sortedByDescending { it.calories }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(elves: List<Elf>): Int = elves.first().calories
+
+    fun part2(elves: List<Elf>): Int = elves.take(3).sumOf { it.calories }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testElves = sortedElves(testInput)
+    check(part1(testElves) == 24000)
+    check(part2(testElves) == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val elves = sortedElves(input)
+    println(part1(elves))
+    println(part2(elves))
 }
